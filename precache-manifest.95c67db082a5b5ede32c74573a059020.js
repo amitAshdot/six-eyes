bb0d9e94dbf3ce9cef3454ef963eef2118942d1e
Amit Ashdot <amitashdot@gmail.com>
2020-08-03 19:04:28 +0300

    Updates

---
+++ precache-manifest.95c67db082a5b5ede32c74573a059020.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0e57ce7d16843fb4be6313d0c4bfd81",
+    "url": "/six-eyes/index.html"
+  },
+  {
+    "revision": "7678be8628e9048a031a",
+    "url": "/six-eyes/static/css/main.817d7f80.chunk.css"
+  },
+  {
+    "revision": "0e3456b1a6f85f7d2e2f",
+    "url": "/six-eyes/static/js/2.3b22050c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/six-eyes/static/js/2.3b22050c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7678be8628e9048a031a",
+    "url": "/six-eyes/static/js/main.bf5152ac.chunk.js"
+  },
+  {
+    "revision": "97efe89cef8a70f74600",
+    "url": "/six-eyes/static/js/runtime-main.d01551d3.js"
+  },
+  {
+    "revision": "c4cea1c3f81ffc3ea8df7bda146b5462",
+    "url": "/six-eyes/static/media/JS.c4cea1c3.png"
+  },
+  {
+    "revision": "4912f6d800e1b75354e0b5d4794e1856",
+    "url": "/six-eyes/static/media/Redux.4912f6d8.png"
+  },
+  {
+    "revision": "f2282f94c041c37ef4db028e428e41c1",
+    "url": "/six-eyes/static/media/html.f2282f94.png"
+  },
+  {
+    "revision": "4af927e2fc1041447294de7e5cbcd446",
+    "url": "/six-eyes/static/media/reactIcon.4af927e2.png"
+  },
+  {
+    "revision": "eac0fdba26c36d995b451da52e901668",
+    "url": "/six-eyes/static/media/sass.eac0fdba.png"
+  }
+]);
